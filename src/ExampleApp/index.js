ee890317534f55e708667e3a0790af674946a8d3
Brian Salazar <bsalazarsanchez06@gmail.com>
2020-08-27 17:40:03 -0600

    Add radio buttons and checkbox code (#3)
    * add radio buttons and checkbox
    
    * modify checkbox to scss
    
    * add new css in the bv styles

--- src/ExampleApp/index.js
+++ src/ExampleApp/index.js
@@ -1,6 +1,8 @@
 import 'bootstrap/scss/bootstrap.scss';
 import 'bootstrap/dist/js/bootstrap';
 import '../CSSLibrary/BeenVerified/buttons.scss';
+import '../CSSLibrary/BeenVerified/radioButtons.scss';
+import '../CSSLibrary/BeenVerified/checkbox.scss';
 import './css/styles.scss';
 import { initialize } from './js/runner';
 
